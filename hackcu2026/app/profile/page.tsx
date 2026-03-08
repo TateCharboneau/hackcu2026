ad6b5a618e3c3e74770d92a661d7fd33669ed586
Matveybu <bubalo.matvey@gmail.com>
2026-03-07 22:01:11 -0700

    profile page

---
+++ hackcu2026/app/profile/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Container, Title, Paper, Stack, Group, Avatar, Text, Divider } from "@mantine/core";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { IconUser, IconMail, IconCalendar } from "@tabler/icons-react";
+
+
+export default function ProfilePage() {
+    const { data: session, status } = useSession();
+
+    // Redirect to home if not authenticated
+    if (status === "loading") {
+        return null;
+    }
+
+    if (!session?.user) {
+        redirect("/");
+    }
+
+    return (
+        <Container size="md" py="xl">
+            <Stack gap="lg">
+                {/* Profile Header */}
+                <Paper shadow="sm" p="xl" radius="md">
+                    <Group gap="lg">
+                        <Avatar
+                            src={session.user.image}
+                            size={120}
+                            radius="md"
+                        />
+                        <Stack gap="xs">
+                            <Title order={2}>{session.user.name}</Title>
+                            <Group gap="xs">
+                                <IconMail size={18} />
+                                <Text c="dimmed">{session.user.email}</Text>
+                            </Group>
+                        </Stack>
+                    </Group>
+                </Paper>
+
+                {/* Account Information */}
+                <Paper shadow="sm" p="xl" radius="md">
+                    <Title order={3} mb="md">Account Information</Title>
+                    <Stack gap="md">
+                        <Group justify="apart">
+                            <Group gap="xs">
+                                <IconUser size={18} />
+                                <Text fw={500}>Name</Text>
+                            </Group>
+                            <Text>{session.user.name}</Text>
+                        </Group>
+                        <Divider />
+                        <Group justify="apart">
+                            <Group gap="xs">
+                                <IconMail size={18} />
+                                <Text fw={500}>Email</Text>
+                            </Group>
+                            <Text>{session.user.email}</Text>
+                        </Group>
+                    </Stack>
+                </Paper>
+
+
+            </Stack>
+        </Container>
+    );
+}
